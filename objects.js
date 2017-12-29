c4b0a35aed156a4c2f0060b3fb9d180cd8aa5c53
Ariel Ross <arielross92@gmail.com>
2017-12-29 01:27:27 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,7 +3,7 @@ var playlist = {
   artistname2: 'song title2'
 }
 
-function updatePlaylist (playlist,artist,title){
-  playlist.artist = 'title'
+function updatePlaylist (playlist,artistName,songTitle){
+  playlist.artistName = 'songTitle'
   return playlist
 }
